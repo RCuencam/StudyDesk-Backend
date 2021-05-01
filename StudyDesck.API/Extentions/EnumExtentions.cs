689057660b5084afcd8ed23a1113890a3b4bc609
Rodrigo <rcuenca446@gmail.com>
2021-04-30 22:52:18 -0500

    First Version

---
+++ StudyDesck.API/Extentions/EnumExtentions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Reflection;
+using System.ComponentModel;
+
+namespace StudyDesck.API.Extensions
+{
+    static class EnumExtentions
+    {
+        public static string ToDescriptionString<T>(this T @enum) { 
+            FieldInfo info = @enum.GetType().GetField(@enum.ToString());
+            var attributes = (DescriptionAttribute[])info.GetCustomAttributes(typeof(DescriptionAttribute), false); 
+
+            return attributes?[0].Description ?? @enum.ToString();
+        }
+    }
+}
