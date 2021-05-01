d0047bb2692aca31cd102ac0725c2b5eb79f8c36
by-German <german280599@gmail.com>
2021-05-01 00:26:55 -0500

    adding nunit

---
+++ StudyDesck.API.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace StudyDesck.API.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
