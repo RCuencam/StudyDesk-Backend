9229c9b7c6c4f0afbd963b312cdc3d442aae6746
by-German <german280599@gmail.com>
2021-05-03 22:23:08 -0500

    test(dev): Topic and Course implemented

---
+++ StudyDesck.API.Test/CourseServiceTest.cs
@@ -0,0 +1,202 @@
+﻿using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using StudyDesck.API.Domain.Models;
+using StudyDesck.API.Domain.Persistence.Repositories;
+using StudyDesck.API.Domain.Services.Comunications;
+using StudyDesck.API.Persistence.Repositories;
+using StudyDesck.API.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyDesck.API.Test
+{
+    class CourseServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public async Task GetAllAsyncWhenNoCoursesReturnsEmptyCollection()
+        {
+            // Arrange
+            var mockUnitOfWork = GetDefaultUnitOfWorkRepositoryInstance();
+            var mockCourseRepository = GetDefaultCourseRepositoryInstance();
+
+            mockCourseRepository.Setup(r => r.ListAsync()).ReturnsAsync(new List<Course>());
+            var service = new CourseService(mockCourseRepository.Object, mockUnitOfWork.Object);
+
+            // Act
+            List<Course> result = (List<Course>)await service.ListAsync();
+            var courseCount = result.Count;
+
+            // Assert
+            courseCount.Should().Equals(0);
+        }
+
+        
+        [Test]
+        public async Task GetByIdAsyncWhenInvalidIdReturnsCourseNotFoundResponse()
+        {
+            // Arrange
+            var mockUnitOfWork = GetDefaultUnitOfWorkRepositoryInstance();
+            var mockCourseRepository = GetDefaultCourseRepositoryInstance();
+            var CourseId = 1;
+
+            mockCourseRepository.Setup(r => r.FindById(CourseId)).Returns(Task.FromResult<Course>(null));
+            var service = new CourseService(mockCourseRepository.Object, mockUnitOfWork.Object);
+
+            // Act
+            CourseResponse result = await service.GetByIdAsync(CourseId);
+            var message = result.Message;
+
+            // Assert
+            message.Should().Be("course not found");
+        }
+
+        [Test]
+        public async Task GetByIdAsyncWheIdIsCorrectReturnsCourseInstance()
+        {
+            // Arrange
+            var mockUnitOfWork = GetDefaultUnitOfWorkRepositoryInstance();
+            var mockCourseRepository = GetDefaultCourseRepositoryInstance();
+            var CourseId = 1;
+            var course = new Course()
+            {
+                Id = 1,
+                Name = "string",
+                CareerId = 1
+            };
+
+            mockCourseRepository.Setup(r => r.FindById(CourseId)).Returns(Task.FromResult(course));
+            var service = new CourseService(mockCourseRepository.Object, mockUnitOfWork.Object);
+
+            // Act
+            CourseResponse result = await service.GetByIdAsync(CourseId);
+            var resource = result.Resource;
+
+            // Assert
+            resource.Should().Equals(course);
+        }
+
+        [Test]
+        public async Task UpdateAsyncWhenIdIsCorrectReturnsCourseInstance()
+        {
+            // Arrange
+            var mockUnitOfWork = GetDefaultUnitOfWorkRepositoryInstance();
+            var mockCourseRepository = GetDefaultCourseRepositoryInstance();
+            var CourseId = 1;
+            var course = new Course()
+            {
+                Id = 1,
+                Name = "string",
+                CareerId = 1
+            };
+
+            mockCourseRepository.Setup(r => r.FindById(CourseId)).Returns(Task.FromResult(course));
+            mockCourseRepository.Setup(r => r.Update(course));
+            var service = new CourseService(mockCourseRepository.Object, mockUnitOfWork.Object);
+
+            // Act
+            CourseResponse result = await service.UpdateAsync(CourseId, course);
+            var resource = result.Resource;
+
+            // Assert
+            resource.Should().Equals(course);
+        }
+
+        [Test]
+        public async Task UpdateAsyncWhenInvalidIdReturnsCourseNotFoundResponse()
+        {
+            // Arrange
+            var mockUnitOfWork = GetDefaultUnitOfWorkRepositoryInstance();
+            var mockCourseRepository = GetDefaultCourseRepositoryInstance();
+            var CourseId = 1;
+            var course = new Course()
+            {
+                Id = 1,
+                Name = "string",
+                CareerId = 1
+            };
+
+            mockCourseRepository.Setup(r => r.FindById(CourseId)).Returns(Task.FromResult<Course>(null));
+            mockCourseRepository.Setup(r => r.Update(course));
+            var service = new CourseService(mockCourseRepository.Object, mockUnitOfWork.Object);
+
+            // Act
+            CourseResponse result = await service.UpdateAsync(CourseId, course);
+            var message = result.Message;
+
+            // Assert
+            message.Should().Be("Course not found");
+        }
+
+        [Test]
+        public async Task DeleteAsyncWhenInvalidIdReturnsCourseNotFoundResponse()
+        {
+            // Arrange
+            var mockUnitOfWork = GetDefaultUnitOfWorkRepositoryInstance();
+            var mockCourseRepository = GetDefaultCourseRepositoryInstance();
+            var CourseId = 1;
+            var course = new Course()
+            {
+                Id = 1,
+                Name = "string",
+                CareerId = 1
+            };
+
+            mockCourseRepository.Setup(r => r.FindById(CourseId)).Returns(Task.FromResult<Course>(null));
+            mockCourseRepository.Setup(r => r.Remove(course));
+            var service = new CourseService(mockCourseRepository.Object, mockUnitOfWork.Object);
+
+            // Act
+            CourseResponse result = await service.DeleteAsync(CourseId);
+            var message = result.Message;
+
+            // Assert
+            message.Should().Be("Course not found");
+        }
+
+        [Test]
+        public async Task DeleteAsyncWhenIdIsCorrectReturnsCourseInstance()
+        {
+            // Arrange
+            var mockUnitOfWork = GetDefaultUnitOfWorkRepositoryInstance();
+            var mockCourseRepository = GetDefaultCourseRepositoryInstance();
+            var CourseId = 1;
+            var course = new Course()
+            {
+                Id = 1,
+                Name = "string",
+                CareerId = 1
+            };
+
+            mockCourseRepository.Setup(r => r.FindById(CourseId)).Returns(Task.FromResult(course));
+            mockCourseRepository.Setup(r => r.Remove(course));
+            var service = new CourseService(mockCourseRepository.Object, mockUnitOfWork.Object);
+
+            // Act
+            CourseResponse result = await service.DeleteAsync(CourseId);
+            var instance = result.Resource;
+
+            // Assert
+            instance.Should().Be(course);
+        }
+
+        private Mock<ICourseRepository> GetDefaultCourseRepositoryInstance()
+        {
+            return new Mock<ICourseRepository>();
+        }
+        private Mock<IUnitOfWork> GetDefaultUnitOfWorkRepositoryInstance()
+        {
+            return new Mock<IUnitOfWork>();
+        }
+
+    }
+}
---
+++ StudyDesck.API.Test/TopicServiceTest.cs
@@ -0,0 +1,200 @@
+﻿using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using StudyDesck.API.Domain.Models;
+using StudyDesck.API.Domain.Persistence.Repositories;
+using StudyDesck.API.Domain.Services.Comunications;
+using StudyDesck.API.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyDesck.API.Test
+{
+    class TopicServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public async Task GetAllAsyncWhenNoTopicsReturnsEmptyCollection()
+        {
+            // Arrange
+            var mockUnitOfWork = GetDefaultUnitOfWorkRepositoryInstance();
+            var mockTopicRepository = GetDefaultTopicRepositoryInstance();
+
+            mockTopicRepository.Setup(r => r.ListAsync()).ReturnsAsync(new List<Topic>());
+            var service = new TopicService(mockTopicRepository.Object, mockUnitOfWork.Object);
+
+            // Act
+            List<Topic> result = (List<Topic>)await service.ListAsync();
+            var topicCount = result.Count;
+
+            // Assert
+            topicCount.Should().Equals(0);
+        }
+
+
+        [Test]
+        public async Task GetByIdAsyncWhenInvalidIdReturnsTopicNotFoundResponse()
+        {
+            // Arrange
+            var mockUnitOfWork = GetDefaultUnitOfWorkRepositoryInstance();
+            var mockTopicRepository = GetDefaultTopicRepositoryInstance();
+            var TopicId = 1;
+
+            mockTopicRepository.Setup(r => r.FindById(TopicId)).Returns(Task.FromResult<Topic>(null));
+            var service = new TopicService(mockTopicRepository.Object, mockUnitOfWork.Object);
+
+            // Act
+            TopicResponse result = await service.GetByIdAsync(TopicId);
+            var message = result.Message;
+
+            // Assert
+            message.Should().Be("Topic not found");
+        }
+
+        [Test]
+        public async Task GetByIdAsyncWheIdIsCorrectReturnsTopicInstance()
+        {
+            // Arrange
+            var mockUnitOfWork = GetDefaultUnitOfWorkRepositoryInstance();
+            var mockTopicRepository = GetDefaultTopicRepositoryInstance();
+            var TopicId = 1;
+            var topic = new Topic()
+            {
+                Id = 1,
+                Name = "string",
+                CourseId = 1
+            };
+
+            mockTopicRepository.Setup(r => r.FindById(TopicId)).Returns(Task.FromResult(topic));
+            var service = new TopicService(mockTopicRepository.Object, mockUnitOfWork.Object);
+
+            // Act
+            TopicResponse result = await service.GetByIdAsync(TopicId);
+            var resource = result.Resource;
+
+            // Assert
+            resource.Should().Equals(topic);
+        }
+
+        [Test]
+        public async Task UpdateAsyncWhenIdIsCorrectReturnsTopicInstance()
+        {
+            // Arrange
+            var mockUnitOfWork = GetDefaultUnitOfWorkRepositoryInstance();
+            var mockTopicRepository = GetDefaultTopicRepositoryInstance();
+            var TopicId = 1;
+            var topic = new Topic()
+            {
+                Id = 1,
+                Name = "string",
+                CourseId = 1
+            };
+
+            mockTopicRepository.Setup(r => r.FindById(TopicId)).Returns(Task.FromResult(topic));
+            mockTopicRepository.Setup(r => r.Update(topic));
+            var service = new TopicService(mockTopicRepository.Object, mockUnitOfWork.Object);
+
+            // Act
+            TopicResponse result = await service.UpdateAsync(TopicId, topic);
+            var resource = result.Resource;
+
+            // Assert
+            resource.Should().Equals(topic);
+        }
+
+        [Test]
+        public async Task UpdateAsyncWhenInvalidIdReturnsTopicNotFoundResponse()
+        {
+            // Arrange
+            var mockUnitOfWork = GetDefaultUnitOfWorkRepositoryInstance();
+            var mockTopicRepository = GetDefaultTopicRepositoryInstance();
+            var TopicId = 1;
+            var topic = new Topic()
+            {
+                Id = 1,
+                Name = "string",
+                CourseId = 1
+            };
+
+            mockTopicRepository.Setup(r => r.FindById(TopicId)).Returns(Task.FromResult<Topic>(null));
+            mockTopicRepository.Setup(r => r.Update(topic));
+            var service = new TopicService(mockTopicRepository.Object, mockUnitOfWork.Object);
+
+            // Act
+            TopicResponse result = await service.UpdateAsync(TopicId, topic);
+            var message = result.Message;
+
+            // Assert
+            message.Should().Be("Topic not found");
+        }
+
+        [Test]
+        public async Task DeleteAsyncWhenInvalidIdReturnsTopicNotFoundResponse()
+        {
+            // Arrange
+            var mockUnitOfWork = GetDefaultUnitOfWorkRepositoryInstance();
+            var mockTopicRepository = GetDefaultTopicRepositoryInstance();
+            var TopicId = 1;
+            var topic = new Topic()
+            {
+                Id = 1,
+                Name = "string",
+                CourseId = 1
+            };
+
+            mockTopicRepository.Setup(r => r.FindById(TopicId)).Returns(Task.FromResult<Topic>(null));
+            mockTopicRepository.Setup(r => r.Remove(topic));
+            var service = new TopicService(mockTopicRepository.Object, mockUnitOfWork.Object);
+
+            // Act
+            TopicResponse result = await service.DeleteAsync(TopicId);
+            var message = result.Message;
+
+            // Assert
+            message.Should().Be("Topic not found");
+        }
+
+        [Test]
+        public async Task DeleteAsyncWhenIdIsCorrectReturnsTopicInstance()
+        {
+            // Arrange
+            var mockUnitOfWork = GetDefaultUnitOfWorkRepositoryInstance();
+            var mockTopicRepository = GetDefaultTopicRepositoryInstance();
+            var TopicId = 1;
+            var topic = new Topic()
+            {
+                Id = 1,
+                Name = "string",
+                CourseId = 1
+            };
+
+            mockTopicRepository.Setup(r => r.FindById(TopicId)).Returns(Task.FromResult(topic));
+            mockTopicRepository.Setup(r => r.Remove(topic));
+            var service = new TopicService(mockTopicRepository.Object, mockUnitOfWork.Object);
+
+            // Act
+            TopicResponse result = await service.DeleteAsync(TopicId);
+            var instance = result.Resource;
+
+            // Assert
+            instance.Should().Be(topic);
+        }
+
+        private Mock<ITopicRepository> GetDefaultTopicRepositoryInstance()
+        {
+            return new Mock<ITopicRepository>();
+        }
+        private Mock<IUnitOfWork> GetDefaultUnitOfWorkRepositoryInstance()
+        {
+            return new Mock<IUnitOfWork>();
+        }
+    }
+}
