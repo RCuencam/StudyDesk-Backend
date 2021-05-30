18bf020e0a4474791acd61a7265906fd60cfee26
Carlos Izarra <eduizarra2@hotmail.com>
2021-05-30 13:56:43 -0500

    refactor: Uncloupe sessionReservationController in studentSessionsController and sessionStudentsController

---
+++ StudyDesck.API/Controllers/SessionStudentsController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using StudyDesck.API.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyDesck.API.Controllers
+{
+    [ApiController]
+    [Route("/api/sessions/{sessionId}/students")]
+    [Produces("application/json")]
+    public class SessionStudentsController : ControllerBase
+    {
+        private readonly ISessionReservationService _sessionReservationService;
+        private readonly IStudentService _studentService;
+        private readonly ISessionService _sessionService;
+        private readonly IMapper _mapper;
+
+        public SessionStudentsController(ISessionReservationService sessionReservationService, IStudentService studentService, ISessionService sessionService, IMapper mapper)
+        {
+            _sessionReservationService = sessionReservationService;
+            _studentService = studentService;
+            _sessionService = sessionService;
+            _mapper = mapper;
+        }
+    }
+}
